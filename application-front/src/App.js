f734954fd7aaab10ea51d87c335b4e72626bf0de
Elphège <elphege78@gmail.com>
2024-07-20 01:08:36 +0200

    pageUserDossiersV1

--- application-front/src/App.js
+++ application-front/src/App.js
@@ -44,7 +44,7 @@ function App() {
               />
 
               <Route
-                  path="/user"
+                  path="/user/:userID"
                   element={
                       <SecurityRoute requiredRole="user">
                           <UserPage onLogout={handleLogout} />
--- application-front/src/Components/UserPage/UserPage.js
+++ application-front/src/Components/UserPage/UserPage.js
@@ -1,11 +1,39 @@
 import "./UserPage.css";
 // Importation des bibliothèques nécessaires
-
+import { useLocation } from "react-router-dom";
+import Document from "../Document/Document";
 
 const UserPage = () => {
+    const location = useLocation();
+    const { state } = location;
+    const username = state.username || "'Utilisateur non identifié'";
+    const collection = state.collection || [];
+
+    //appel API pour le téléchargement
+ const handleFolderClick = (folderId) => {
+     console.log(`Dossier cliqué: ${folderId}`);
+ };
+
+
     return (
         <>
-            <h1>Page utilisateur</h1>
+            <h1>Bonjour {username},</h1>
+            <h2>Voici les partitions que nous mettons à ta disposition !</h2>
+
+            <ul className="container">
+                {collection.length === 0 ? (
+                    <li>Aucun utilisateur disponible</li>
+                ) : (
+                    collection.map((user, index) => (
+                        <li key={index}>
+                            <Document
+                                titre={user}
+                                telecharg={()=> handleFolderClick(index)}
+                            />
+                        </li>
+                    ))
+                )}
+            </ul>
         </>
     );
 };
