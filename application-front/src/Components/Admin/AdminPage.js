396b7f11ca829875ae566ffed3f6bd0011c8c469
Elphège <elphege78@gmail.com>
2024-07-21 15:14:16 +0200

    dragAndDropV1

--- application-front/src/Components/Admin/AdminPage.js
+++ application-front/src/Components/Admin/AdminPage.js
@@ -3,6 +3,7 @@ import "./AdminPage.css";
 // import React, { useState, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
 import DocumentAdmin from "../Admin/DocumentAdmin";
+import UserCard from "../Admin/UserCard";
 import { useLocation } from "react-router-dom";
 import imgTrash from "../../images/trash-can-solid.svg";
 import imgPlus from "../../images/plus-solid (1).svg";
@@ -12,23 +13,68 @@ const AdminPage = () => {
     const { state } = location;
     const username = state.username || "'Utilisateur non identifié'";
     const collection = state.collection || [];
+    const listUsers = state.listUsers || [];
 
+        
+        const showList = (index) =>  {
+            let popup= document.getElementById("popup"+index);
+           popup.style.display = "block";
+        }
+        const hideList = (index) =>  {
+            let popup= document.getElementById("popup"+index);
+           popup.style.display = "none";
+        }
+            // drag and drop functions
+         const onDragStart = (event) => {
+             event.dataTransfer.setData("Text", event.target.id);
+         };
+
+          const onDragOver = (event) => {
+              event.preventDefault();
+          };
+        // drop function
+        const onDrop = (event, targetItem) => {
+            event.preventDefault();
+            const draggedItemId = event.dataTransfer.getData("Text");
+            console.log(draggedItemId, targetItem);
+            //ici remplacer le console.log ci-dessus en effectuant un Create de l'enregistrement ayant  user.id_user comme identifiant et en utilisant "draggedItemTitle" qui est l'ID du fichier à ajouter à cette entrée! (onDrop deviendra une fonction async) dans la table associative (users-fichiers)
+        }
+        
+
+
+    // Simulate API call to save the dragged item for the target user
     return (
-        <>
+        <div className="fond">
             <h1>Bonjour {username}</h1>
             <div className="present">
                 <div className="align">
-                    <p>Bienvenue sur l'interface d'administration de ta partothèque!</p>
+                    <p>
+                        Bienvenue sur l'interface d'administration de ta
+                        partothèque!
+                    </p>
                     <p>Ici tu pourras: </p>
-                    <ul >
-                        <li>Ajouter/ Enlever des partitions de ta partothèque</li>
-                        <li>Ajouter/ Enlever des utilisateurs avec qui tu veux partager les partitions</li>
-                        <li>Partager tes partitions avec les utilisateurs (en faisant un "glisserr/déposer' de la parttition sur l'utilisateur) </li>
+                    <ul>
+                        <li>
+                            Ajouter/ Enlever des partitions de ta partothèque
+                        </li>
+                        <li>
+                            Ajouter/ Enlever des utilisateurs avec qui tu veux
+                            partager les partitions
+                        </li>
+                        <li>
+                            Partager tes partitions avec les utilisateurs (en
+                            faisant un "glisser/déposer' de la partition sur
+                            l'utilisateur)
+                        </li>
+                        <li>
+                            Visualiser (au survol) les partitions disponibles
+                            dans la partothèque des utilisateurs
+                        </li>
                     </ul>
                 </div>
             </div>
             <main>
-                <ul className="containerAdmin">
+                <div className="containerAdmin">
                     <div className="CD">
                         <button
                             className="plus"
@@ -39,16 +85,25 @@ const AdminPage = () => {
                         <img className="trash" src={imgTrash} alt="poubelle" />
                     </div>
                     <h2 className="admin">Partitions</h2>
-                    {collection.length === 0 ? (
-                        <li>Aucun utilisateur disponible</li>
-                    ) : (
-                        collection.map((user, index) => (
-                            <li key={index}>
-                                <DocumentAdmin titre={user} />
+                    <ul className="listDoc">
+                        {collection.length === 0 ? (
+                            <li className="noData">
+                                Aucun utilisateur disponible
                             </li>
-                        ))
-                    )}
-                </ul>
+                        ) : (
+                            collection.map((user, index) => (
+                                <li
+                                    draggable="true"
+                                    key={"collection"+index}
+                                    onDragStart={onDragStart}
+                                    id={user[1]}
+                                >
+                                    <DocumentAdmin titre={user[0]} />
+                                </li>
+                            ))
+                        )}
+                    </ul>
+                </div>
                 <div className="containerUser">
                     <div className="CD">
                         <button
@@ -60,9 +115,33 @@ const AdminPage = () => {
                         <img className="trash" src={imgTrash} alt="poubelle" />
                     </div>
                     <h2 className="admin">Liste d'utilisateurs</h2>
+                    <ul>
+                        {listUsers.length === 0 ? (
+                            <li className="noData">
+                                Aucun utilisateur disponible
+                            </li>
+                        ) : (
+                            listUsers.map((user, index) => (
+                                <li
+                                    key={"listUsers"+index}
+                                    onMouseEnter={() => showList(index)}
+                                    onMouseLeave={() => hideList(index)}
+                                    onDragOver={onDragOver}
+                                    onDrop={(event) => onDrop(event, {user})}
+                                    id={user.id_user}
+                                >
+                                    <UserCard
+                                        alias={user.name}
+                                        id={index}
+                                        titres={user.titres}
+                                    />
+                                </li>
+                            ))
+                        )}
+                    </ul>
                 </div>
             </main>
-        </>
+        </div>
     );
 };
 export default AdminPage;
--- application-front/src/Components/Admin/DocumentAdmin.js
+++ application-front/src/Components/Admin/DocumentAdmin.js
@@ -6,11 +6,12 @@ import img from "../../images/document-icon-18.png";
 const Document = ({titre,telecharg}) => {
     return (
         <>
-            <div className="cardAdmin">
+            <div className="cardAdmin" >
                 <img
                     src={img}
                     alt="document"
                     onClick={telecharg}
+                    draggable="false"
                 />
                 <p className="doc">{titre}</p>
             </div>
